8be199973853fa63503894d5032f14ea7170191d
Vera <vera.voloshina.andreeva@gmail.com>
2020-09-15 22:15:32 +0300

    Задания к третьему уроку

--- exercise_3_3.py
+++ exercise_3_3.py
@@ -1,23 +1,3 @@
-# def my_func(arg_1, arg_2, arg_3):
-#     result_list = []
-#     const_args_list = [arg_1, arg_2, arg_3]
-#     var_args_list = [arg_1, arg_2, arg_3]
-#     max_arg = max(const_args_list)
-#     for element in range(len(const_args_list)):
-#         if max_arg == const_args_list[element]:
-#             result_list.append(const_args_list[element])
-#             var_args_list.remove(const_args_list[element])
-#             if len(var_args_list) == 1:
-#                 break
-#             else:
-#                 # result_list.append(max_element)
-#                 max_arg = max(var_args_list)
-#         else:
-#             continue
-#     if len(result_list) != 2:
-#
-#     return sum(result_list)
-
 def max_element_from_list(numbers_list):
     """Возвращаем максимальное значение из списка"""
     return max(numbers_list)
